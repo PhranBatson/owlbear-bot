208d6ab162dda35a71fea257a29dcaf6634783ae
PhranBatson <FrancisOBatson@gmail.com>
2023-02-21 22:37:42 -0500

    fixed dependencies

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
